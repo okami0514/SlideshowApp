7274e8eb75ee7fc777d031536315cd200bff59d8
okami-ap <y.okami@alphapolis.co.jp>
2023-09-16 09:13:22 +0900

    戻る、進むボタンアクション実装 再生ボタン押下時、戻る、進むボタンの非活性実装

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let imageName = ["150x150_1.png", "150x150_2.png", "150x150_3.png"]
+    var changeImgNo = 0
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var goOnButton: UIButton!
+    @IBOutlet weak var bachButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        image.image = UIImage(named: imageName[0])
+    }
+    
+    @IBAction func play(_ sender: Any) {
+        goOnButton.isEnabled = false
+        bachButton.isEnabled = false
+    }
+    // 戻るボタン押下時
+    @IBAction func back(_ sender: Any) {
+        
+        if changeImgNo == 0 {
+            changeImgNo = 2
+        } else if changeImgNo == 1 {
+            changeImgNo = 0
+        } else if changeImgNo == 2 {
+            changeImgNo = 1
+        }
+        let name = imageName[changeImgNo]
+        image.image = UIImage(named: name)
     }
 
-
+    // 進むボタン押下時
+    @IBAction func goOn(_ sender: Any) {
+        
+        if changeImgNo == 0 {
+            changeImgNo = 1
+        } else if changeImgNo == 1 {
+            changeImgNo = 2
+        } else if changeImgNo == 2 {
+            changeImgNo = 0
+        }
+        let name = imageName[changeImgNo]
+        image.image = UIImage(named: name)
+    }
 }
 
